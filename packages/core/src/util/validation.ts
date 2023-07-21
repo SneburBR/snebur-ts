66a6de2aa1d291501a59e9d2b9f784eb1f05cd7d
Rubens Cordeiro <zyoncore@hotmail.com>
2023-07-21 18:34:30 -0300

    added isValidNumber to util/validation

--- packages/core/src/util/validation.ts
+++ packages/core/src/util/validation.ts
@@ -1,4 +1,18 @@
 
+/**
+ * Checks if a value is a string.
+ * @param value The value to check.
+ * @returns True if the value is a string, false otherwise.
+ */
 export function isString(value: unknown): value is string {
-  return typeof value === 'string';
+  return typeof value === "string";
+}
+
+/**
+ * Checks if a value is a valid number.
+ * @param value The value to check.
+ * @returns True if the value is a valid number, false otherwise.
+ */
+export function isValidNumber(value: unknown): value is number {
+  return typeof value === "number" && !isNaN(value) && isFinite(value);
 }
--- packages/core/tests/util/validation.test.ts
+++ packages/core/tests/util/validation.test.ts
@@ -1,28 +1,44 @@
 import { describe, expect, it } from "vitest";
-import { isString } from "@snebur/core/src/util/validation";
+import { isString, isValidNumber } from "@snebur/core/src/util/validation";
 
-describe("isString", () => {
-    it("should return true for a string", () => {
+describe("validation", () => {
+
+  describe("isString", () => {
+    it("should return true for a string value", () => {
       expect(isString("hello")).toBe(true);
     });
 
-    it("should return false for a number", () => {
+    it("should return false for a non-string value", () => {
       expect(isString(123)).toBe(false);
-    });
-  
-    it("should return false for an object", () => {
+      expect(isString(true)).toBe(false);
       expect(isString({})).toBe(false);
-    });
-  
-    it("should return false for an array", () => {
       expect(isString([])).toBe(false);
-    });
-  
-    it("should return false for null", () => {
       expect(isString(null)).toBe(false);
-    });
-  
-    it("should return false for undefined", () => {
       expect(isString(undefined)).toBe(false);
     });
   });
+
+  describe("isValidNumber", () => {
+    it("should return true for a valid number value", () => {
+      expect(isValidNumber(123)).toBe(true);
+      expect(isValidNumber(0)).toBe(true);
+      expect(isValidNumber(-123)).toBe(true);
+      expect(isValidNumber(1.23)).toBe(true);
+    });
+
+    it("should return false for a non-number value", () => {
+      expect(isValidNumber("123")).toBe(false);
+      expect(isValidNumber(true)).toBe(false);
+      expect(isValidNumber({})).toBe(false);
+      expect(isValidNumber([])).toBe(false);
+      expect(isValidNumber(null)).toBe(false);
+      expect(isValidNumber(undefined)).toBe(false);
+    });
+
+    it("should return false for an invalid number value", () => {
+      expect(isValidNumber(NaN)).toBe(false);
+      expect(isValidNumber(Infinity)).toBe(false);
+      expect(isValidNumber(-Infinity)).toBe(false);
+    });
+  });
+});
