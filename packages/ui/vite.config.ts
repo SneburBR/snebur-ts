a68c4cb604c8bb83f94d067c8c4a0805760ac140
Rubens Cordeiro <zyoncore@hotmail.com>
2023-07-21 18:36:12 -0300

    added tsconfig and vite.config to snebur/ui

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,29 @@
+import { builtinModules } from "module";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    build: {
+        target: "es2015",
+        lib: {
+            entry: [
+                "src/index.ts"
+            ],
+            name: "@snebur/ui",
+            fileName: "index",
+        },
+        commonjsOptions: {
+            ignore: [...builtinModules, "ws"],
+        },
+        sourcemap: true,
+        outDir: "build",
+        emptyOutDir: true,
+        minify: false,
+    },
+    test: {
+        globals: true,
+        threads: false,
+        setupFiles: "tests/setup.ts",
+        logHeapUsage: true,
+    },
+});
+
