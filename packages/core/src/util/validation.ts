f8493a7ce21beb618b6a69b8b150210b5f3c6bee
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-13 16:56:06 -0300

    added isBase64 to ValidationUtil

--- packages/core/src/util/validation.ts
+++ packages/core/src/util/validation.ts
@@ -1,4 +1,6 @@
 
+const base64Regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+
 /**
  * Checks if a value is a string.
  * @param value The value to check.
@@ -8,6 +10,7 @@ export function isString(value: unknown): value is string {
   return typeof value === "string";
 }
 
+
 /**
  * Checks if a value is a valid number.
  * @param value The value to check.
@@ -16,3 +19,13 @@ export function isString(value: unknown): value is string {
 export function isValidNumber(value: unknown): value is number {
   return typeof value === "number" && !isNaN(value) && isFinite(value);
 }
+
+/**
+ * Determines whether a given string is a valid base64 string.
+ * @param value The string to check.
+ * @returns True if the string is a valid base64 string, false otherwise.
+ */
+export function isBase64(value: string): boolean {
+  return isString(value) && base64Regex.test(value);
+}
+
