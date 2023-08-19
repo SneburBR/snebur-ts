3ce8b898de9519d5584f9f049229934e3c016110
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-19 20:07:13 -0300

    updated ecma script version

--- packages/ui-demo/vite.config.ts
+++ packages/ui-demo/vite.config.ts
@@ -3,27 +3,21 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     build: {
-        target: "es2022",
+        target: "es2015",
         lib: {
             entry: [
-                "src/index.ts"
+                "src/main.ts"
             ],
-            name: "@snebur/core",
-            fileName: "index",
+            name: "@snebur/ui-demo",
+            fileName: "main",
         },
         commonjsOptions: {
-            ignore: [...builtinModules ],
+            ignore: [...builtinModules, "ws"],
         },
         sourcemap: true,
         outDir: "build",
         emptyOutDir: true,
         minify: false,
-    },
-    test: {
-        globals: true,
-        threads: false,
-        setupFiles: "tests/setup.ts",
-        logHeapUsage: true,
     } 
 });
 
