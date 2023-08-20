845dc12ba2032af3bfcefd4efee50db942a04e4a
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-20 15:53:19 -0300

    updated util/index

--- packages/core/src/util/index.ts
+++ packages/core/src/util/index.ts
@@ -1,11 +1,17 @@
-import * as FileUtil from "./file";
-import * as TextUtil from "./text";
-import * as ValidationUtil from "./validation";
 import * as ConvertUtil from "./convert";
+import * as DateUtil from "./date";
+import * as FileUtil from "./file";
+import * as FormatUtil from "./format";
+import * as NormalizeUtil from "./normalize";
+import * as ValidationUtil from "./validation";
+import * as TextUtil from "./text";
 
 export {       
+    ConvertUtil,
+    DateUtil,
+    FileUtil,
+    FormatUtil,
+    NormalizeUtil,
     ValidationUtil,
     TextUtil,
-    FileUtil,
-    ConvertUtil,
 };
