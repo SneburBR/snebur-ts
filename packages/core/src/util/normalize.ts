71a9692b8ac6592fb0df21e8e8ae0ae314b4c884
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-20 15:33:02 -0300

    added normalizeDate to normalizeUtil

--- packages/core/src/util/normalize.ts
+++ packages/core/src/util/normalize.ts
@@ -1,3 +1,4 @@
+import { getMonthDays } from "./date";
 import { SpecialCharsOptions, getOnlyNumbers, isNullOrEmpty, isNullOrWhiteSpace } from "./text";
 
 /**
@@ -6,10 +7,10 @@ import { SpecialCharsOptions, getOnlyNumbers, isNullOrEmpty, isNullOrWhiteSpace 
  * @returns The normalized number as a string with a dot as the decimal separator.
  */
 export function normalizeNumber(number: number | string): string {
-   
+
     if (isNullOrWhiteSpace(number?.toString())) return "";
 
-    const numbersAndPoints = getOnlyNumbers(number.toString(), SpecialCharsOptions.PointComma);
+    const numbersAndPoints = getOnlyNumbers(number.toString(), SpecialCharsOptions.PointCommaSigns);
     const lastPointIndex = Math.max(numbersAndPoints.lastIndexOf("."), numbersAndPoints.lastIndexOf(","));
 
     if (lastPointIndex === -1) return numbersAndPoints;
@@ -17,4 +18,84 @@ export function normalizeNumber(number: number | string): string {
     const decimalPart = numbersAndPoints.substring(lastPointIndex + 1);
     const integerPart = numbersAndPoints.substring(0, lastPointIndex).replace(/\.|,/g, "");
     return `${integerPart}.${decimalPart}`;
+}
+
+/**
+ * Normalizes a date string or Date object to a string in the format "dd/mm/yyyy".
+ * @param value - The date string or Date object to normalize.
+ * @returns A string in the format "dd/mm/yyyy" representing the normalized date.
+ */
+export function normalizeDate(value: string | Date, isFormatZero:boolean = false): string {
+
+    if (value instanceof Date) {
+
+        if(isFormatZero){
+            return value.toLocaleDateString();
+        }
+        return `${value.getDate()}/${value.getMonth() + 1}/${value.getFullYear()}`;
+    }
+        
+
+    if (isNullOrEmpty(value?.toString())) return "";
+
+    const separators = ["\\/", "\\-", "\\."];
+    const dateParts = value.toString().split(new RegExp(`[${separators.join("")}]`));
+    const dayOrYear = parseInt(dateParts[0]);
+    const maybeMonth = parseInt(dateParts[1]);
+    const yearOrDay = parseInt(dateParts[2]);
+
+    if (isNaN(dayOrYear) && isNaN(maybeMonth) && isNaN(yearOrDay))
+        return "";
+
+    const [day, month, year] = normalizeDatePartsInternal(dayOrYear, maybeMonth, yearOrDay);
+    const monthNormalized = Math.min(month, 12);
+    const dayInMonth = getMonthDays(year, monthNormalized - 1);
+    const dayNormalized = Math.min(day, dayInMonth);
+    if(isFormatZero)
+        return `${formatNumberInternal(dayNormalized)}/${formatNumberInternal(monthNormalized)}/${year}`;
+    return `${dayNormalized}/${monthNormalized}/${year}`;
+}
+
+function normalizeDatePartsInternal(dayOrYear: number, maybeMonth: number, yearOrDay: number): [number, number, number] {
+
+    const currentDate = new Date();
+
+    if (isNaN(maybeMonth) && isNaN(yearOrDay)) {
+
+        if (dayOrYear > 31)
+            return [currentDate.getDate(), currentDate.getMonth() + 1, dayOrYear];
+
+        return [dayOrYear, currentDate.getMonth() + 1, currentDate.getFullYear()];
+    }
+
+    if (isNaN(yearOrDay)) {
+
+        if (dayOrYear > 31)
+            return [currentDate.getDate(), maybeMonth, dayOrYear];
+
+        return [dayOrYear, maybeMonth, currentDate.getFullYear()];
+    }
+
+    if (dayOrYear > 31) {
+        return [yearOrDay, maybeMonth, dayOrYear];
+    }
+
+    const year = normalizeYear(yearOrDay);
+    return [dayOrYear, maybeMonth, year];
+}
+
+function normalizeYear(year: number): number {
+
+    if (year < 50)  
+        return year + 2000;
+  
+    if (year < 100) 
+        return year + 1900;
+
+    return year;
+}
+
+
+function formatNumberInternal(value : number): string {
+    return value.toString().padStart(2, "0");
 }
--- packages/core/tests/util/normalize.test.ts
+++ packages/core/tests/util/normalize.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
-import { normalizeNumber } from "../../src/util/normalize";
+import { normalizeNumber, normalizeDate } from "../../src/util/normalize";
+import { getMonthDays } from "../../src/util/date";
 
 describe("NormalizeUtil", () => {
 
@@ -11,8 +12,8 @@ describe("NormalizeUtil", () => {
             expect(normalizeNumber(undefined)).toBe("");
             expect(normalizeNumber(true as any)).toBe("");
             expect(normalizeNumber(false as any)).toBe("");
-            expect(normalizeNumber({}as any)).toBe("");
-            expect(normalizeNumber([]as any)).toBe("");
+            expect(normalizeNumber({} as any)).toBe("");
+            expect(normalizeNumber([] as any)).toBe("");
         });
 
         it("should return an empty string if the input is not a number or a string representation of a number", () => {
@@ -22,6 +23,54 @@ describe("NormalizeUtil", () => {
             expect(normalizeNumber("123hello123")).toBe("123123");
             expect(normalizeNumber("123.456.789")).toBe("123456.789");
             expect(normalizeNumber("123,456,789")).toBe("123456.789");
+        });
+    });
+
+    describe("normalizeDate", () => {
+
+        it("should return an empty string if the input is null or undefined", () => {
+            expect(normalizeDate("")).toBe("");
+            expect(normalizeDate(null)).toBe("");
+            expect(normalizeDate(undefined)).toBe("");
+            expect(normalizeDate(true as any)).toBe("");
+            expect(normalizeDate(false as any)).toBe("");
+            expect(normalizeDate({} as any)).toBe("");
+            expect(normalizeDate([] as any)).toBe("");
+        });
+
+        it("should return an empty string if the input is not a date or a string representation of a date", () => {
+            expect(normalizeDate("hello")).toBe("");
+            expect(normalizeDate("hello123")).toBe("");
+        });
+
+        //it("should return a string representation of the date if the input is a  date", () => {
+        it("should return a string representation of the date if the input is a partial date string", () => {
+
+            const currentDate = new Date();
+            const day = currentDate.getDate();
+            const month = currentDate.getMonth() + 1;
+            const year = currentDate.getFullYear();
+            const daysInFebruary = getMonthDays(year, 1);
+             
+            expect(normalizeDate("1")).toBe(`1/${month}/${year}`);
+            expect(normalizeDate("2020")).toBe(`${day}/${month}/2020`);
+            expect(normalizeDate("1/1")).toBe(`1/1/${year}`);
+            expect(normalizeDate("1/1", true)).toBe(`01/01/${year}`);
+            expect(normalizeDate("1/4")).toBe(`1/4/${year}`);
+            expect(normalizeDate("31/2")).toBe(`${daysInFebruary}/2/${year}`);
+            expect(normalizeDate("1/1/20")).toBe("1/1/2020");
+            expect(normalizeDate("1/4/80")).toBe("1/4/1980");
+            expect(normalizeDate("1/1/2020")).toBe("1/1/2020");
+            expect(normalizeDate("1/4/1980")).toBe("1/4/1980");
+            expect(normalizeDate("1/1/2020", true)).toBe("01/01/2020");
+            expect(normalizeDate("1/4/1980", true)).toBe("01/04/1980");
+        });
+
+        it("should return a string representation of the date if the input is a date", () => {
+            expect(normalizeDate(new Date(2020, 0, 1))).toBe("1/1/2020");
+            expect(normalizeDate(new Date(2020, 0, 1), true)).toBe("01/01/2020");
+            expect(normalizeDate(new Date(1980, 3, 1))).toBe("1/4/1980");
+            expect(normalizeDate(new Date(1980, 3, 1), true)).toBe("01/04/1980");
         });
     });
 });
